8e8279a2cbcf452d8de85edeeda40a42df5b181d
Si-ri-sha <sirishasirisha806@gmail.com>
2025-04-09 18:35:31 +0530

    feat: added solution to sort string elements in an array in js

---
+++ BasicConcepts/sorting_strings.js
@@ -0,0 +1,7 @@
+//function in js to sort a array consiisting of strings
+
+function sortAlphabetically(arr) {
+  return arr.sort(); //by default sort is built-in to sort alphabetically
+}
+
+console.log(sortAlphabetically(["B", "N", "C" , "S", "L" ]));
